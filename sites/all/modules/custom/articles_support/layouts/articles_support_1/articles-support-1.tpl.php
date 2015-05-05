de97775a072edb98a2e28d0ffad22d20524d6d2c
Larry <larry@thethreestooges.com.au>
2015-05-05 12:49:51 -0700

    finished with article full view mode in ds

--- sites/all/modules/custom/articles_support/layouts/articles_support_1/articles-support-1.tpl.php
+++ sites/all/modules/custom/articles_support/layouts/articles_support_1/articles-support-1.tpl.php
@@ -2,17 +2,61 @@
 
 /**
  * @file
- * Display Suite 1 column template.
+ * articles support v1 full node layout template.
  */
 ?>
-<<?php print $article_content_wrapper; print $layout_attributes; ?> class="article_content_1 <?php print $classes;?> clearfix">
 
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="articles-support-1 <?php print $classes;?> clearfix">
   <?php if (isset($title_suffix['contextual_links'])): ?>
-  <?php print render($title_suffix['contextual_links']); ?>
+    <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
-  <?php print $article_content; ?>
-</<?php print $article_content_wrapper ?>>
+  <<?php print $header_wrapper ?> class="<?php print $header_classes;?>">
+    <h2 class="title"><?php print $title ?></h2>
+    <?php print $header; ?>
+  </<?php print $header_wrapper ?>>
+
+  <<?php print $row1_wrapper ?> class="<?php print $row1_classes;?>">
+    <?php print $row1; ?>
+  </<?php print $row1_wrapper ?>>
+
+  <<?php print $row2_wrapper ?> class="<?php print $row2_classes;?>">
+    <<?php print $row2_col1_wrapper ?> class="<?php print $row2_col1_classes;?>">
+     <?php print $row2_col1; ?>
+    </<?php print $row2_col1_wrapper ?>>
+
+    <<?php print $row2_col2_wrapper ?> class="<?php print $row2_col2_classes;?>">
+      <?php print $row2_col2; ?>
+    </<?php print $row2_col2_wrapper ?>>
+
+    <?php print $row2; ?>
+  </<?php print $row2_wrapper ?>>
+
+  <<?php print $row3_wrapper ?> class="<?php print $row3_classes;?>">
+    <?php print $row3; ?>
+  </<?php print $row3_wrapper ?>>
+
+  <<?php print $row_mobile_wrapper ?> class="<?php print $row_mobile_classes;?>">
+    <<?php print $row_mobile_col1_wrapper ?> class="<?php print $row_mobile_col1_classes;?>">
+      <?php print $row_mobile_col1; ?>
+    </<?php print $row_mobile_col1_wrapper ?>>
+
+    <<?php print $row_mobile_col2_wrapper ?> class="<?php print $row_mobile_col2_classes;?>">
+      <?php print $row_mobile_col2; ?>
+    </<?php print $row_mobile_col2_wrapper ?>>
+
+    <?php print $row_mobile; ?>
+  </<?php print $row_mobile_wrapper ?>>
+
+  <<?php print $row5_wrapper ?> class="<?php print $row5_classes;?>">
+    <?php print $row5; ?>
+  </<?php print $row5_wrapper ?>>
+
+  <<?php print $row_bottom_wrapper ?> class="<?php print $row_bottom_classes;?>">
+    <?php print $row_bottom; ?>
+  </<?php print $row_bottom_wrapper ?>>
+
+</<?php print $layout_wrapper ?>>
 
 <?php if (!empty($drupal_render_children)): ?>
   <?php print $drupal_render_children ?>
