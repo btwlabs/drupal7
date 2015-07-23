0505a4ad717bcd31b33ca9e1579ce96617d5a0a8
Larry <ted@btwlabs.com>
2015-07-22 17:28:21 -0700

    progress on waypoints ui

--- sites/all/modules/custom/waypoints_ui/js/waypoints_ui.js
+++ sites/all/modules/custom/waypoints_ui/js/waypoints_ui.js
@@ -4,19 +4,31 @@
 (function($){
     Drupal.behaviors.waypoints_ui = {
         attach:	function(context, settings) {
+            var waypoints = new Object;
             for (var key in settings.waypointsUI) {
-                waypoint = settings.waypointsUI[key];
+                // Constructing an array of waypoints keyed by attach selector.
+                // This makes finding the correct toggle pieces easier later.
+                var waypoint = settings.waypointsUI[key];
+                waypoints[waypoint.attach_element.replace(/^\./, "")] = waypoint;
                 var attachSelector = waypoint.attach_element;
-                var toggleClass = waypoint.toggle_class;
-                var toggleOffset = waypoint.toggle_offset;
-                if (waypoint.same_element == 1) {
-                    var toggleSelector = waypoint.attach_element;
-                }
-                else {
-                    var toggleSelector = waypoint.toggle_element;
-                }
+                var toggleOffset =  waypoint.toggle_offset;
                 $(attachSelector, context).waypoint(function() {
-                    $(toggleSelector).toggleClass(toggleClass);
+                    // Have to figure out which element was fired on.
+                    // Go through each class of the triggered element and find a match
+                    // in the waypoints array.
+                    for (var key in this.classList) {
+                        var thisClass = this.classList[key];
+                        if (!(typeof waypoints[thisClass] === 'undefined')) {
+                            var thisWayPoint = waypoints[thisClass];
+                        }
+                    }
+                    if (thisWayPoint.same_element == 1) {
+                        var toggleSelector = thisWayPoint.attach_element;
+                    }
+                    else {
+                        var toggleSelector = thisWayPoint.toggle_element;
+                    }
+                    $(toggleSelector).toggleClass(thisWayPoint.toggle_class);
 
                 }, {
                     offset: toggleOffset + '%'
